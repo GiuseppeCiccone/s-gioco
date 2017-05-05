dd3eef05fe43d12917c2fb265bc0a94ff964d727
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2017-05-05 09:50:19 +0200

    mods

--- api/server.js
+++ api/server.js
@@ -1,14 +1,43 @@
 'use strict';
+const pkg     = require('./package.json');
 const express = require('express');
-const app = new express();
+const mysql   = require('mysql');
+const datasource = require('./datasource.json');
 
-app.get('/ciaone', (req, res) => {
-   console.log(req.headers);
-   res.json(req.headers);
+const app = new express();
+const startDate = new Date();
+
+const poolConfig = Object.assign(datasource, {
+    connectionLimit : 10
 });
+console.log(poolConfig);
+const pool = mysql.createPool(poolConfig);
 
 app.get('/', (req, res)=> {
-    res.send('<h1>ciao! sono la home page!</h1>')
+    res.json({
+        name: pkg.name,
+        version: pkg.version,
+        startDate,
+        uptime: new Date() - startDate
+    });
+});
+
+app.get('/articles', (req, res) => {
+    let limit;
+    try {
+        limit = Number(req.query.limit) || 10;
+    } catch(err) {
+        limit = 10;
+    }
+
+    pool.query(`SELECT * FROM articles LIMIT ${limit}`, (err, results) => {
+        if (err) return res.status(500).json({ error: err });
+
+        res.json({
+            count: results.length,
+            data: results
+        })
+    });
 });
 
 app.all('*', (req, res) => {
