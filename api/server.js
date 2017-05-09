d4e3ac59b5ea47d76da6b647f22b11e644270ddf
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2017-05-09 12:53:14 +0200

    mods

--- api/server.js
+++ api/server.js
@@ -1,28 +1,19 @@
 'use strict';
-const pkg     = require('./package.json');
+const pkg    = require('./package.json');
 const express = require('express');
-const mysql   = require('mysql');
+const mysql  = require('mysql');
 const datasource = require('./datasource.json');
 
 const app = new express();
 const startDate = new Date();
 
 const poolConfig = Object.assign(datasource, {
-    connectionLimit : 10
+   connectionLimit : 10
 });
 console.log(poolConfig);
 const pool = mysql.createPool(poolConfig);
 
-app.get('/', (req, res)=> {
-    res.json({
-        name: pkg.name,
-        version: pkg.version,
-        startDate,
-        uptime: new Date() - startDate
-    });
-});
-
-app.get('/articles', (req, res) => {
+app.all('*', (req, res, next) => {
     let limit;
     try {
         limit = Number(req.query.limit) || 10;
@@ -30,23 +21,61 @@ app.get('/articles', (req, res) => {
         limit = 10;
     }
 
-    pool.query(`SELECT * FROM articles LIMIT ${limit}`, (err, results) => {
-        if (err) return res.status(500).json({ error: err });
+    if (limit > 100) limit = 100;
 
-        res.json({
-            count: results.length,
-            data: results
-        })
-    });
+    let skip;
+    try {
+        skip = Number(req.query.skip) || 0;
+    } catch(err) {
+        skip = 0;
+    }
+
+    req.limit = limit;
+    req.skip = skip;
+    next();
 });
 
-app.all('*', (req, res) => {
-   res.status(404).json( {
-       status: 404,
-       message: 'Pagina non trovata'
+app.get('/', (req, res)=> {
+   res.json({
+      name: pkg.name,
+      version: pkg.version,
+      startDate,
+      uptime: new Date() - startDate
    });
 });
 
-app.listen(8000, () => {
-    console.log('Server in ascolto sulla porta 8000')
+expose('tags');
+expose('articles');
+expose('articles_tags_th');
+
+app.all('*', (req, res) => {
+  res.status(404).json( {
+     status: 404,
+     message: 'Pagina non trovata'
+  });
 });
+
+app.listen(8000, () => {
+   console.log('Server in ascolto sulla porta 8000')
+});
+
+function formatResponse(req, data) {
+    const { limit, skip } = req;
+    return {
+        count: data.length,
+        skip,
+        limit,
+        prevSkip: skip - limit < 0? 0 : skip - limit,
+        nextSkip: skip + limit,
+        data
+    };
+}
+
+function expose(tableName) {
+    app.get(`/${tableName}`, (req, res) => {
+        pool.query(`SELECT * FROM ${tableName} LIMIT ${req.limit} OFFSET ${req.skip}`, (err, results) => {
+            if (err) return res.status(500).json({ error: err });
+            res.json(formatResponse(req, results));
+        });
+    });
+}
