aa98c8f04b27397cfcf35cabd620468d3861db63
MatteCarra <MatteCarra@users.noreply.github.com>
2018-02-02 10:01:44 +0100

    Update db.js

--- db.js
+++ db.js
@@ -7,72 +7,74 @@ const poolConfig = Object.assign(datasource, {
     connectionLimit : 10
 });
 
+const e = module.exports = {};
+
 const pool = mysql.createPool(poolConfig);
 
-exports.list = function(tableName, limit, skip, cb) {
+const list = e.list = (tableName, limit, skip, cb) => {
     pool.query(`SELECT * FROM ${tableName} LIMIT ${limit} OFFSET ${skip}`, cb);
 }
 
-exports.get = function(tableName, alias, cb) {
+const get = e.get = (tableName, alias, cb) => {
     pool.query(`SELECT * FROM ${tableName} WHERE alias=?`, alias, cb);
 }
 
-exports.insert = function(tableName, body, cb) {
+const insert = e.insert = (tableName, body, cb) => {
     pool.query(`INSERT INTO ${tableName} SET ?`, body, cb);
 }
 
-exports.update = function(tableName, alias, body, cb) {
+const update = e.update = (tableName, alias, body, cb) => {
     pool.query(`UPDATE ${tableName} SET ? WHERE alias=?`, [body, alias], cb);
 }
 
-exports.deleteRow = function(tableName, alias, cb) {
+const deleteRow = e.deleteRow = (tableName, alias, cb) => {
     pool.query(`DELETE FROM ${tableName} WHERE alias=?`, alias, cb);
 }
 
-exports.getArticle = function(alias, cb) {
+const getUser = e.getUser = (username, password, cb) => {
+    pool.query('SELECT username,permissions FROM users WHERE username=? and password=?', [username, sha256(password)], cb);
+}
+
+const getArticle = e.getArticle = (alias, cb) => {
 	return get('articles', alias, cb);
 }
 
-exports.listArticles = function(limit, skip, cb) {
+const listArticles = e.listArticles = (limit, skip, cb) => {
     return list('articles', limit, skip, cb);
 }
 
-exports.insertArticle = function(body, cb) {
+const insertArticle = e.insertArticle = (body, cb) => {
 	return insert('articles', body, cb);
 }
 
-exports.updateArticle = function(alias, body, cb) {
+const updateArticle = e.updateArticle = (alias, body, cb) => {
     return update('articles', alias, body, cb);
 }
 
-exports.deleteArticle = function(alias, cb) {
+const deleteArticle = e.deleteArticle = (alias, cb) => {
     return insert('articles', alias, cb);
 }
 
-exports.listTags = function(limit, skip, cb) {
+const listTags = e.listTags = (limit, skip, cb) => {
     return list('tags', limit, skip, cb);
 }
 
-exports.getTag = function(alias, cb) {
+const getTag = e.getTag = (alias, cb) => {
     return get('tags', alias, cb);
 }
 
-exports.insertTag = function(body, cb) {
+const insertTag = e.insertTag = (body, cb) => {
     return insert('tags', body, cb);
 }
 
-exports.updateTag = function(alias, body, cb) {
+const updateTag = e.updateTag = (alias, body, cb) => {
     return update('tags', alias, body, cb);
 }
 
-exports.deleteTag = function(alias, cb) {
+const deleteTag = e.deleteTag = (alias, cb) => {
     return insert('tags', alias, cb);
 }
 
-exports.listArticlesTagsTh = function(limit, skip, cb) {
+const listArticlesTagsTh = e.listArticlesTagsTh = (limit, skip, cb) => {
     return list('articles_tags_th', limit, skip, cb);
-}
-
-exports.getUser = function(username, password, cb) {
-    pool.query('SELECT username,permissions FROM users WHERE username=? and password=?', [username, sha256(password)], cb);
 }
